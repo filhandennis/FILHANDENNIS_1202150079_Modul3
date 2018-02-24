d9fa720cb0a0c5f88e0f1b2fe57a08f8c3fc013d
Xandor <filhandennis@gmail.com>
2018-02-24 12:51:19 +0700

    No Comment

---
+++ app/src/main/java/com/lesson/filhan/filhandennis_1202150079_modul3/Splashscreen.java
@@ -0,0 +1,22 @@
+package com.lesson.filhan.filhandennis_1202150079_modul3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 3500);
+    }
+}
